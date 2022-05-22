2bbc02b1da6422ea513ab8d0e8cc0ca24872f071
Diyorbek03 <dqadamboyev117@gmail.com>
2022-05-22 13:02:39 +0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const mony = Number(prompt("Summani kiriting"));
+
+const monyAqsh = Number(750*10896);
+
+const monyEvr = Number(120*12247);
+
+
+
+if(mony > monyAqsh + monyEvr) {
+    console.log("Oq yo’l, Alisher!");
+} else {
+    console.log("Alisher, ozgina sabr qilish kerak bo’lar ekan");
+}
